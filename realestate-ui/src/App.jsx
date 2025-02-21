b0c855eb45704ecfac28a033662653386dc18020
rabbkhan <rabbilkhan125@gmail.com>
2025-02-22 01:43:13 +0530

    estate ui home design

---
+++ realestate-ui/src/App.jsx
@@ -0,0 +1,53 @@
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import ListPage from "./routes/listPage/ListPage";
+import Layout from "./routes/layout/layout";
+import SinglePage from "./routes/singlePage/SinglePage";
+import ProfilePage from "./routes/profilePage/profilePage";
+import Login from "./routes/login/Login";
+import Register from "./routes/register/Register";
+import HomePage from "./routes/homepage/HomePage";
+
+function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout />,
+      children:[
+        {
+          path:"/",
+          element:<HomePage/>
+        },
+        {
+          path:"/list",
+          element:<ListPage/>
+        },
+        // {
+        //   path:"/:id",
+        //   element:<SinglePage/>
+        // },
+        {
+          path:"/profile",
+          element:<ProfilePage/>
+        },
+        {
+          path:"/login",
+          element:<Login/>
+        },
+        {
+          path:"/register",
+          element:<Register/>
+        }
+      ]
+    }
+  ]);
+
+  return (
+
+    <RouterProvider router={router}/>
+  );
+}
+
+export default App;
---
+++ realestate-ui/src/routes/listPage/ListPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const listPage = () => {
+  return (
+    <div>
+      Listpage
+    </div>
+  )
+}
+
+export default listPage
